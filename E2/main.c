bfa357fa93f92848d211eec1d3b08b6d9844e51b
Beatriz da Cruz Bacelar <69614234+BeatrizBacelar@users.noreply.github.com>
2024-08-09 21:45:09 -0300

    Update main.c

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,22 @@
 /* main.c */
+#include <stdio.h>
+#include "token.h"
 
+extern int yylex();
+
+int main () {
+// nome da funçao retorna o proximo token é yylex()
+
+    token_t token = -1;
+    while (token != EOL){
+        token = yylex();                    // call yylex()
+        if(token == ERROR  ){
+            printf("error lexico\n");
+        }
+        else
+        printf("Código do token: %d\n",token);
+    }
+
+
+
+}
